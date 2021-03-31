76540f29ba329797452b2e3edb58190815bce777
Harry Dhillon <navbirsdhillon@gmail.com>
2021-03-30 21:52:32 -0600

    home page update

--- client/src/components/homeStyles.js/homeStyles.js
+++ client/src/components/homeStyles.js/homeStyles.js
@@ -1,22 +1,21 @@
 import styled from 'styled-components';
 import {Button} from 'semantic-ui-react';
 const HomeTitle = styled.h1`
-  background: linear-gradient(to bottom right, purple, grey);
+  background: linear-gradient(to bottom right, purple, black);
   color: white;
+  padding: 10px;
   text-align: center;
-  font-family: rustic
+  font-family: 'Parisienne', cursive;
 `
 export const HomeBody = styled.p`
   text-align: center;
   padding: 20px;
   border: 1px solid;
-  font-family: century gothic;
   font-size: 17px;
 `
 export const MyButton = styled(Button)`
 border-radius: 25px !important;  
 color: purple !important;
-
 `
 
 
--- client/src/components/shared/Home.js
+++ client/src/components/shared/Home.js
@@ -1,22 +1,25 @@
 import HomeTitle, { HomeBody } from '../homeStyles.js/homeStyles';
 import {Link} from 'react-router-dom';
-import {Button} from 'semantic-ui-react';
+import {Button, Container, Divider, Header, Icon } from 'semantic-ui-react';
 const Home = () => {
 return(
   <>
+  <Container textAlign='center'>
+    <br />
+    <HomeTitle> Welcome to Devpoint Workers</HomeTitle><Divider /> <br /> <br />
+    <HomeBody>At Devpoint Workers, we are here to give you the best service possible.
+      We garantee that we have the right worker for you.
+    </HomeBody>
+    <Header size="large">Please browse through our Workers to select which specialist can help you.</Header>
+   
+      <Button size='big' icon labelPosition='right'>
+        <Link to="/workers" >
+        Our Workers
+        </Link>
+        <Icon name='right arrow' />
+      </Button>
   
-  <HomeTitle> Welcome to Devpoint Workers</HomeTitle>
-  <HomeBody>At Devpoint Workers, we are here to give you the best service possible.
-    We garantee that we have the right worker for you.
-  </HomeBody>
-  <p style={{textAlign:"center"}}>Please browse through our Workers to select which specialist can help you.</p>
-  <div style={{textAlign:"center"}}>
-    <Button size="small" basic color="purple">
-      <Link to="/workers" >
-      Our Workers
-      </Link>
-    </Button>
-  </div>
+  </Container>
   </>
 )
 }
