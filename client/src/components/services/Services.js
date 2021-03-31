90dee5dd9ee67c8386f6b51ba3aeaf9ec5b84337
Harry Dhillon <navbirsdhillon@gmail.com>
2021-03-30 20:41:57 -0600

    service edit addded

--- client/src/components/services/Services.js
+++ client/src/components/services/Services.js
@@ -20,6 +20,20 @@ class Services extends Component {
       })
       .catch( err => console.log(err))
   }
+  updateService = (serviceId, service ) => {
+    const { id } = this.props.match.params
+    axios.put(`/api/workers/${id}/services/${serviceId}`, { service })
+    .then( res => {
+      const services = this.state.services.map( s => {
+        if(s.id === serviceId){
+          return res.data
+        }
+        return s
+      })
+      this.setState({services})
+    })
+    .catch(err => console.log(err))
+  }
   deleteService = (serviceId) => {
     const { id } = this.props.match.params
     axios.delete(`/api/workers/${id}/services/${serviceId}`)
@@ -34,7 +48,7 @@ class Services extends Component {
     return (
       <>
         { services.map( s => 
-          <Service key={s.id} {...s} deleteService={this.deleteService} />
+          <Service key={s.id} {...s} updateService={this.updateService} deleteService={this.deleteService} />
         )}
       </>
     )
